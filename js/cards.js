53f107d2eed6d09f16ec8026708cb2a2118344ee
Nishant Sinha <nishnha@gmail.com>
2016-10-22 17:33:52 -0400

    added animations

--- js/cards.js
+++ js/cards.js
@@ -1,10 +1,10 @@
 var source   = $("#card-template").html();
 var template = Handlebars.compile(source);
 
-function card(img, name, inOut) {
-    var context  = {img: img, name: name, inOut: inOut};
-    $("#card-container").append(template(context));
+function card(img, name, front, back) {
+    var context  = {img: img, name: name, isIn: name + " last checked in at " + front, lastIn: "Last checked out at " + back};
+    $("#center-cards").append(template(context));
 }
 
-card("res/avatar.jpg", "Kevin Aiken", "Checked in since");
-card("res/avatar.jpg", "Nishant Sinha", "hes not here right now");
+card("res/KevinAiken.png", "Kevin Aiken", "9:05", "4:20");
+card("res/avatar.jpg", "Nishant Sinha", "6:30", "8:20");
